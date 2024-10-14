5c04d47a9993b10124046368706833415587b6e1
MarLen887 <147438967+MarLen887@users.noreply.github.com>
2024-10-14 16:53:30 -0600

    Add files via upload

---
+++ app/Http/Controllers/TostadosController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Tostados;
+use Illuminate\Http\Request;
+
+class TostadosController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Tostados $tostados)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Tostados $tostados)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Tostados $tostados)
+    {
+        //
+    }
+}
